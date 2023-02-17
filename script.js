a6b524efb08f94094b029cc55f39da5e574f1703
Elvis Diaz <elvisdiaz020@gmail.com>
2023-02-17 16:34:24 -0500

    updates dynamic time block style switching functionality

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-$(function () {
-  // Gets DOM elements 
+$(document).ready(function () {
+  // Gets DOM elements
   let headerEl = $("#header");
   let block = $(".time-block");
+  let body = $("body");
 
-  // Creates Current Date 
+  // Creates Current Date
   let today = dayjs();
   let currentDay = today.format("MMMM D, YYYY");
   let date = ($("#currentDay").text = currentDay);
+
   headerEl.append(date);
-      
-      // Gets Hour in 24hr format
+
+  // Gets Hour in 24hr format
   let currentHour = today.format("H");
 
   // Loops the divs inside the container of time blocks
@@ -18,27 +20,40 @@ $(function () {
     let blockHour = $(this).attr("id");
     // Determines the style of each time block depending on the current time
     if (blockHour < currentHour) {
-      block.attr("class", "row time-block past");
+      $(this).attr("class", "row time-block past");
     } else if (blockHour == currentHour) {
-      block.attr("class", "row time-block present");
+      $(this).attr("class", "row time-block present");
     } else {
-      block.attr("class", "row time-block future");
+      $(this).attr("class", "row time-block future");
     }
-    
-    // Gets the save button and text area 
-    let noteEl = $(this).find(".saveBtn");
-    let textArea = $(this).find(".description");
 
-    // event listener when save button is clicked
-    noteEl.on("click", function (event) {
-      event.preventDefault();
-      localStorage.setItem("note", textArea.val());
-    });
-
-    
-     
+    // console.log(blockHour < currentHour)
   });
-  textArea.val() = localStorage.getItem('note')
+
+  // Gets the save button and text area
+  let noteEl = block.find(".saveBtn");
+  console.log(noteEl);
+  let textArea = $(this).find(".description");
+
+  // event listener when save button is clicked
+  $(".saveBtn").on("click", function (event) {
+    let note = $(this).siblings(".description").val();
+    let hour = $(this).parent().attr("id");
+    event.preventDefault();
+    console.log("click");
+
+    localStorage.setItem(hour, note);
+
+    // window.location.reload();
+  });
+
+  window.onload = $("09").find(".description").val(localStorage.getItem("09"));
+  window.onload = $("10").find(".description").val(localStorage.getItem("10"));
+  window.onload = $("11").find(".description").val(localStorage.getItem("11"));
+  window.onload = $("12").find(".description").val(localStorage.getItem("12"));
+  window.onload = $("13").find(".description").val(localStorage.getItem("13"));
+  window.onload = $("14").find(".description").val(localStorage.getItem("14"));
+  window.onload = $("15").find(".description").val(localStorage.getItem("15"));
+  window.onload = $("16").find(".description").val(localStorage.getItem("16"));
+  window.onload = $("17").find(".description").val(localStorage.getItem("17"));
 });
-
-
